09f754d98b6aaa1fd30437187de77e1de1bdaa8f
Cecil Verghese <cecil.verghese@gmail.com>
2018-01-30 04:23:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
-  console.log(`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
