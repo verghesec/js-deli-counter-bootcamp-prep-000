68013c2ebc2b10cbe8f5917f4aa6899a7a63a021
Cecil Verghese <cecil.verghese@gmail.com>
2018-01-30 04:40:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
+
+function nowServing(katzDeliLine) {
+  var current = katzDeliLine.shift();
+  if (current) {
+    return `Currently serving ${current}.`;
+  }
+  else {
+    return "There is nobody waiting to be served!";
+  }
+}
